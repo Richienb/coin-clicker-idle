d1bc44b07e94e9846ab268374efbcb66320c43bc
Richie Bendall <richiebendall@gmail.com>
2018-06-13 20:31:00 +1200

    Next Commit

--- game.py
+++ game.py
@@ -1,3 +1,8 @@
+from time import sleep as delay
 while True:
     input("Press Enter To Get A Coin")
     print("You Got A Coin!")
+    print("Wait 5 Seconds")
+    delay(5)
+    print("Keep Going!")
+    
