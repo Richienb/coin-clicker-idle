244f13a5dbf78b83bc06735820f48aac72f34e2a
Richie Bendall <richiebendall@gmail.com>
2018-06-14 16:02:35 +1200

    Counting Coins

--- game.py
+++ game.py
@@ -1,12 +1,15 @@
 from time import sleep as delay
+coins = 0
 while True:
     if not(input("Press any key and press enter: ") == ""):
+        coins += 1
         print("You Got A Coin!")
         f = open('coin.txt')
         print("-+yy:-")
         print("-ddNd/h:")
         print(":ddmh/h/")
         print("-+oo::` ")
+        print("You Now Have {0} Coins".format(coins))
         print("Keep Going!")
     else:
         print("You didn't press any key and press enter")
